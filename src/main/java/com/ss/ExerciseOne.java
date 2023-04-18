384d2aef28210cae4077807c58420102d1f41f07
Salha Siddig <129996035+ssiddig@users.noreply.github.com>
2023-04-17 22:23:55 -0400

    ExerciseOne

---
+++ src/main/java/com/ss/ExerciseOne.java
@@ -0,0 +1,17 @@
+package com.ss;
+
+public class ExerciseOne {
+    public static void main(String[] args){
+
+        String name = "Full Name: ";
+        String firstName = " Glen ";
+        String middleName = " C. ";
+        String lastName = " Williams ";
+        String suffix = " PHD ";
+        String results = name + firstName + middleName + lastName + suffix;
+
+        System.out.println(results);
+
+
+    }
+}
--- src/main/java/com/ss/WorkingWithString.java
+++ src/main/java/com/ss/WorkingWithString.java
@@ -2,6 +2,64 @@ package com.ss;
 
 public class WorkingWithString {
     public static void main(String[] args){
-        System.out.println("My superpower is: Time Travel");
+
+
+        ///////////////////
+
+//
+//        int num1 = 10;
+//        int num2 = 10;
+//
+//        System.out.println(num1 == num2);
+//
+//        String phrase1 = "Hello World";
+//        String phrase2 = "Hello World";
+//        String phrase3 = "Hello " + "World";
+//
+//        System.out.println(phrase1 == phrase3);
+        //////////////
+
+//        String myName = "Salha";
+//        String userName = myName.toUpperCase();
+//
+//        System.out.println(userName);
+          ///////////
+//        String phrase = " Hello World ! ";
+//        System.out.println(phrase.trim());
+
+        ///////////
+//
+////        String myPhrase = "Salha is so awesome!!!!!!";
+////        System.out.println(myPhrase.startsWith("sal"));
+//
+//        String myName = "Zayne Bookman";
+//        System.out.println(myName.toLowerCase());
+//
+//        String age = "I am 3 years old";
+//        System.out.println(age);
+//
+//        String parents = "My parents Salha and Anthony";
+//        System.out.println(parents);
+//
+//        String trackingCode = "USA-12981-Y-22";
+//
+//        char hasShipped = trackingCode.charAt(0);
+//        System.out.println(hasShipped);
+        //////////
+
+//        String productCode1 = "ACME-12213";
+//        int dashPosition1 = productCode1.indexOf("-");
+//        String vendor1 = productCode1.substring(0, dashPosition1 =1);
+//
+//        String productCode2 = "SAMSUNG-320233242";
+
+//        String str = "my@dear@aunt@sally";
+////        System.out.println(str.substring(3, 7));
+//        System.out.println(Arrays .toString(str.split("@"));
+
+//        String fName =
+
+
+
     }
 }
